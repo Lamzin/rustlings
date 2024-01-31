b9f5db2111c9730667ca52da8368a15fba5b2e86
Oleh Lamzin <lamzin@google.com>
2024-01-31 18:16:01 +0100

    My progress on 31.01.24

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,8 +16,6 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub struct ReportCard {
     pub grade: f32,
     pub student_name: String,
@@ -27,7 +25,15 @@ pub struct ReportCard {
 impl ReportCard {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+            &self.student_name, &self.student_age, self.grade())
+    }
+
+    fn grade(&self) -> String {
+        if self.grade == 1.0 {
+            "A+".to_string()
+        } else {
+            format!("{}", self.grade)
+        }
     }
 }
 
@@ -52,7 +58,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: 1.0,
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
